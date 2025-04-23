0628e85ecd73abd4e7b860fdb4f5b59ae79fdcd0
ibonbautista <ibonbautista@gmail.com>
2025-04-23 12:50:22 +0200

    Player functions and associations.js

--- src/routes/api/playerRouter.js
+++ src/routes/api/playerRouter.js
@@ -6,6 +6,16 @@ const router = Router();
 
 // conseguir todos los doctores
 router.get("/", playerAPIController.getAll)
+router.get("/team/:team", playerAPIController.getPlayerByTeam)
+router.get("/gender/:gender", playerAPIController.getPlayerByGender)
+router.get("/name/:name", playerAPIController.getPlayerByName)
+router.get("/surname/:surname", playerAPIController.getPlayerBySurname)
+router.get("/year/:year", playerAPIController.getPlayerByYear)
+router.get("/town/:town", playerAPIController.getPlayerByTown)
+router.get("/position/:position", playerAPIController.getPlayerByPosition)
+router.get("/heightplus/:height", playerAPIController.getPlayerByHeightPlus)
+router.get("/heightminus/:height", playerAPIController.getPlayerByHeightMinus)
+router.get("/hand/:hand", playerAPIController.getPlayerByHand)
 router.get("/:id", playerAPIController.getPlayerById)
 
 
